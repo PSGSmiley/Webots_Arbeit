e38717cbbc5e5180d70f8e13ff7d128945d7ebc0
Niklas Heiber <43641640+PSGSmiley@users.noreply.github.com>
2023-12-07 22:53:33 +0100

    Delete ws directory

--- ws/mecanum_robot/controllers/my_controller/build/release/my_controller.d
+++
@@ -1,6 +0,0 @@
-build/release/my_controller.o: my_controller.cpp \
- /home/amr/.ros/webotsR2023b/webots/include/controller/cpp/webots/Robot.hpp \
- /home/amr/.ros/webotsR2023b/webots/include/controller/cpp/webots/Motor.hpp \
- /home/amr/.ros/webotsR2023b/webots/include/controller/cpp/webots/Device.hpp \
- /home/amr/.ros/webotsR2023b/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /home/amr/.ros/webotsR2023b/webots/include/controller/cpp/webots/DistanceSensor.hpp
